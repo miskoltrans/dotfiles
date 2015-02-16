031d6320209f1e49d5ca593af01826570a1fcb50
Tomislav Rakic <tomislavr@nordeus.com>
2015-02-16 03:40:32 +0100

    Automatic commit

--- backup.sh
+++ backup.sh
@@ -8,3 +8,53 @@ echo "Using $DIR"
 
 #~/bin contains custom goodies
 cp -r ~/bin $DIR
+
+#~/.config
+mkdir -p $DIR/.config
+cp -r ~/.config/htop $DIR/.config
+cp -r ~/.config/gtk-3.0 $DIR/.config
+
+# dhex, hex editor
+cp ~/.dhexrc $DIR
+
+# dir_colors
+cp ~/.dir_colors $DIR
+
+# git
+cp ~/.gitconfig $DIR
+
+# gitk
+cp ~/.gitk $DIR
+
+# gtk 2.0
+cp ~/.gtkrc-2.0 $DIR
+
+# irssi
+# cp -r ~/.irssi $DIR
+
+# urxvt
+cp -r ~/.urxvt $DIR
+
+# VIM
+cp -r ~/.vim $DIR
+
+# X
+cp ~/.Xauthority	$DIR
+cp ~/.xbindkeysrc	$DIR
+cp ~/.xinitrc		$DIR
+cp ~/.Xresources 	$DIR
+cp ~/.xscreensaver 	$DIR
+
+# zsh
+# cp -r ~/.rvm   $DIR
+cp ~/.zprofile 	$DIR
+cp ~/.zshrc	$DIR
+cp ~/.zshrc.zni $DIR
+
+# /etc/
+
+
+# Commit to git
+git add -A
+git commit -m "Automatic commit"
+git push
